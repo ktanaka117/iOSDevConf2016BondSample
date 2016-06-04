d7884983b77809af1a882476f58c3ba273f50758
田中賢治 <key.m4096.kung.fu@gmail.com>
2016-06-04 09:29:37 +0900

    Initial Commit

---
+++ iOSDevConf2016BondSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSDevConf2016BondSample
+//
+//  Created by 田中賢治 on 2016/06/04.
+//  Copyright © 2016年 田中賢治. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
